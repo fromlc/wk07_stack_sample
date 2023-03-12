3785f9ac59bfc0b258fa6da41d23500fe0da480e
L Carver <wifihum@gmail.com>
2023-03-12 04:25:00 -0700

    style mods

--- source/IntStack.h
+++ source/IntStack.h
@@ -1,28 +1,32 @@
-// Specification file for the IntStack class
+//------------------------------------------------------------------------------
+// IntStack.h : class declaration/specification
+//------------------------------------------------------------------------------
 #ifndef INTSTACK_H
 #define INTSTACK_H
 
-class IntStack
-{
+//------------------------------------------------------------------------------
+// IntStack
+//------------------------------------------------------------------------------
+class IntStack {
 private:
-   int *stackArray;  // Pointer to the stack array
-   int stackSize;    // The stack size
-   int top;          // Indicates the top of the stack
+    int* stackArray;  // Pointer to the stack array
+    int stackSize;    // The stack size
+    int top;          // Indicates the top of the stack
 
 public:
-   // Constructor
-   IntStack(int);
+    // constructor
+    IntStack(int);
 
-   // Copy constructor
-   IntStack(const IntStack &);
+    // copy constructor
+    IntStack(const IntStack&);
 
-   // Destructor
-   ~IntStack();
-   
-   // Stack operations
-   void push(int);
-   void pop(int &);
-   bool isFull() const;
-   bool isEmpty() const;
-}; 
+    // destructor
+    ~IntStack();
+
+    // stack operations
+    void push(int);
+    void pop(int&);
+    bool isFull() const;
+    bool isEmpty() const;
+};
 #endif 
