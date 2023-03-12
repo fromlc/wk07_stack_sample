e8c2dad616fae49f9bec96d3e8e5a8a2edab3edb
L Carver <wifihum@gmail.com>
2023-03-12 03:26:21 -0700

    style mods

--- source/Pr19-1.cpp
+++ source/Pr19-1.cpp
@@ -1,38 +1,55 @@
-// This program demonstrates the IntStack class.
-#include <iostream>
+//------------------------------------------------------------------------------
+// Pr19-1.cpp
+//
+// Demos a stack of ints with the IntStack class
+// 
+// Author: Tony Gaddis, from Gaddis 9E pp. 1168-1171
+// Modified by: Prof. Linda C
+//------------------------------------------------------------------------------
 #include "IntStack.h"
-using namespace std;
 
-int main()
-{
-   int catchVar; // To hold values popped off the stack
+#include <iostream>
+using std::cout;
 
-   // Define a stack object to hold 5 values.
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+int main() {
+
+   // to hold values popped off the stack
+   int catchVar;
+
+   // define a stack object to hold 5 int values
    IntStack stack(5);
 
-   // Push the values 5, 10, 15, 20, and 25 onto the stack.
+   // push the values onto the stack
    cout << "Pushing 5\n";
    stack.push(5);
+
    cout << "Pushing 10\n";
    stack.push(10);
+
    cout << "Pushing 15\n";
    stack.push(15);
+
    cout << "Pushing 20\n";
    stack.push(20);
+
    cout << "Pushing 25\n";
    stack.push(25);
 
-   // Pop the values off the stack.
-   cout << "Popping...\n";
+   // pop the values off the stack
+   cout << "\nPopping...\n";
    stack.pop(catchVar);
-   cout << catchVar << endl;
+   cout << catchVar << '\n';
    stack.pop(catchVar);
-   cout << catchVar << endl;
+   cout << catchVar << '\n';
    stack.pop(catchVar);
-   cout << catchVar << endl;
+   cout << catchVar << '\n';
    stack.pop(catchVar);
-   cout << catchVar << endl;
+   cout << catchVar << '\n';
    stack.pop(catchVar);
-   cout << catchVar << endl;
+   cout << catchVar << '\n';
+
    return 0;
 }
